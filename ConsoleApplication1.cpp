3de7113c7a3f9dbb6df90f112d0822ee82b6003a
GaoLei <gaol8@chinatelecom.cn>
2020-07-03 15:20:21 +0800

    没啥事

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -8,7 +8,7 @@ int main()
     std::cout << "Hello World!\n";
     std::cout << "Hello World Again!\n";
     std::cout << "Test Easy_Git";
-    //std::cout << "Test Branch";123
+    std::cout << "Test Branch";
     std::cout << "Test Stash";
 }
 
